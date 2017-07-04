4f23c011f6ce8e10fa5895cc3314365fd5b92105
mrajashri3697 <nikitha3697@gmail.com>
2017-07-04 17:02:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,6 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML="New Value";
+
+var img = document.getElementById('madi');
+img.onclick = function (){ img.style.marginLeft="100px"; };
