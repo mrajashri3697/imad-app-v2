13128461db14e7ad479c133940038830a1835f95
mrajashri3697 <nikitha3697@gmail.com>
2017-07-04 19:01:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,5 +23,11 @@ button.onclick =function(){
    request.open('GET','http://mrajashri3697.imad.hasura-app.io/counter',true);
    request.send(null);
     
+};
+
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
+var submit = document.getElementById("submit_btn");
+submit.onclick = function() {
     
 };
