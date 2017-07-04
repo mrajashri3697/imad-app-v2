374431d75ae61aa18a481618d3bfa8ff75f22867
mrajashri3697 <nikitha3697@gmail.com>
2017-07-04 18:20:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 var button = document.getElementById('counter');
 button.onclick =function(){
     
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readystate)
+    }
     
     
     counter =counter + 1;
