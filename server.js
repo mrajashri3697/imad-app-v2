8d8f4ea871305e8da566088b4ce2d642d3b613fd
mrajashri3697 <nikitha3697@gmail.com>
2017-07-04 15:42:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ function createTemplate(data)
             </body>
         </html>
         `;
+        return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
@@ -52,7 +53,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 }); 
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
