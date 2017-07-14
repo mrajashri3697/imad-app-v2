e308d8da35f2d2d73ee1cfc2d38e6ecbe451ecfd
mrajashri3697 <nikitha3697@gmail.com>
2017-07-14 18:36:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/counter',function(req,res){
     
 });
 
-var pool = new Pool(config);
+
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test', function(err,result){
         if(err) res.status(500).send(err.toString());
@@ -118,10 +118,11 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 }); 
 
+var pool = new Pool(config);
 app.get('/user/:userName', function (req, res) {
     
     
-    pool.query("SELECT * from User WHERE Username ="+ req.params.userName, function(err,result){
+    pool.query("SELECT * from User WHERE Username ='"+ req.params.userName + "'", function(err,result){
         if(err) res.status(500).send(err.toString());
         else 
         {
