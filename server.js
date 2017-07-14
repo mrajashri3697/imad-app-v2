13960d0c1be5ee7c2ba18739eb4928388d10fa83
mrajashri3697 <nikitha3697@gmail.com>
2017-07-14 18:56:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.get('/user/:userName', function (req, res) {
             else
             {
                 var userData= result.rows;
-                res.send(JSON.stringify(createUserTemplate(userData)));
+                res.send(createUserTemplate(userData));
             }
             
                 
